1845015754d73e8db568ebe69e1838408f201507
Inigo Jiron <pohatu678@msn.com>
2020-10-31 19:25:23 -0400

    added option for inhouse command to use users from voice channel

--- cogs/misc.py
+++ cogs/misc.py
@@ -11,12 +11,20 @@ class Misc(commands.Cog):
 
     @commands.command(aliases=['ih'],
                       description='Create an in-house from arguments',
-                      help='List players separated by a space.',
+                      help='List players separated by a space. Use \'voice\' to use current voice channel users.',
                       brief='Make in-house')
     async def inhouse(self, ctx, *players):
         maps = ['Haven', 'Bind', 'Split', 'Ascent', 'Icebox']
         colors = [Colour.orange(), Colour.from_rgb(165, 42, 42), Colour.blue(), Colour.from_rgb(255, 255, 0), Colour.teal()]
-        if len(players) % 5 == 0:
+        if len(players) == 1 and players[0].lower() == 'voice':
+            voice_state = ctx.author.voice
+            if voice_state:
+                voice_channel = voice_state.channel
+                voice_members = [m.mention for m in voice_channel.members]
+                await self.inhouse(ctx, *voice_members)
+            else:
+                await ctx.send('You must be in a voice channel.')
+        elif players:
             choice = random.randint(0, len(maps) - 1)
             map_choice = maps[choice]
             map_color = colors[choice]
@@ -26,7 +34,8 @@ class Misc(commands.Cog):
             while len(shuffled_players) != 0:
                 team = []
                 for _ in range(0, 5):
-                    team.append(shuffled_players.pop(0))
+                    if shuffled_players:
+                        team.append(shuffled_players.pop(0))
                 teams.append(team)
 
             embed = Embed(title='Map: ' + map_choice,
@@ -38,6 +47,12 @@ class Misc(commands.Cog):
             await ctx.send(embed=embed)
         else:
             await ctx.send('Players not a multiple of 5.')
+
+    @commands.command(description='Shortcut for \'inhouse voice\' command',
+                      help='See \'inhouse\' help.',
+                      brief='Voice in-house')
+    async def ihv(self, ctx):
+        await self.inhouse(ctx, 'voice')
 
 
 def setup(bot):
