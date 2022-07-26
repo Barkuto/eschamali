79bf29c70a2b56193cb3b4fe49b3b2af7f1dafb1
Inigo Jiron <pohatu678@msn.com>
2022-07-25 20:45:20 -0400

    add emoji filter for racoon emoji, need to find its emoji family

--- util/utils.py
+++ util/utils.py
@@ -16,6 +16,7 @@ EMOJI_PATTERN = re.compile("["
                            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
                            u"\U0001F680-\U0001F6FF"  # transport & map symbols
                            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U0001F99D"             # Racoon Emoji
                            "]+", flags=re.UNICODE)
 
 
